3dfdf9cdc8c96a920c160269143b134f0b93feca
Henry LETELLIER <henry.letellier@epitech.eu>
2024-11-28 00:11:53 +0100

    [ADD] a fully working SFML installer

--- bonus/installer_scripts/install_sfml.sh
+++ bonus/installer_scripts/install_sfml.sh
@@ -4,14 +4,28 @@
 SUDO=/usr/bin/sudo
 
 # The link to the source file
-SOURCE_CODE=https://www.sfml-dev.org/files/SFML-2.6.2-linux-gcc-64-bit.tar.gz
+SOURCE_CODE=https://www.sfml-dev.org/files/SFML-2.6.2-sources.zip
 
 # The file name
-FILE_NAME=SFML-2.6.2-linux-gcc-64-bit.tar.gz
+FILE_NAME=SFML-2.6.2-sources.zip
+
+# The extracted folder name
+FOLDER_NAME=SFML-2.6.2
 
 # The destination folder for the file to extract
 FILE_LOCATION=/tmp/SFML
 FINAL_DOWNLOAD_PATH=$FILE_LOCATION/$FILE_NAME
+FINAL_FOLDER_PATH=$FILE_LOCATION/$FOLDER_NAME
+
+# Boolean values
+TRUE=0
+FALSE=1
+
+# The silent mode (make the commands verbose or not)
+SILENT=$TRUE
+
+# Sleep after the warning message
+SLEEP=$TRUE
 
 # Colour list
 RESET='\033[0m'
@@ -53,6 +67,25 @@ function display_info {
     display_text $BLUE "$1"
 }
 
+function display_warning {
+    display_text $YELLOW "$1"
+}
+
+function display_flag {
+    display_text $MAGENTA "$1"
+}
+
+# The function to relaunch the script as root
+function relaunch_as_root {
+    if [ $EUID -ne 0 ]; then
+        display_error "The script must be run as root."
+        $SUDO $0 $@
+        exit $?
+    else
+        display_success "The script is running as root."
+    fi
+}
+
 # Status tracking
 # The success code
 SUCCESS=0
@@ -80,18 +113,75 @@ function display_status {
     update_global_status $STATUS
 }
 
+# Package warning
+function display_package_warning {
+    display_warning "The following packages are required to install the SFML library:"
+    display_warning "cmake, libfreetype6-dev, libjpeg-dev, libxrandr-dev, libgl1-mesa-dev, libudev-dev, libopenal-dev, libflac-dev, libvorbis-dev, libogg-dev"
+    display_warning "These are the packages for the ubuntu distribution, their name might change depending on the distribution."
+    if [ $SLEEP -eq $FALSE ]; then
+        return
+    fi
+    display_warning "The script will sleep for 5 seconds before starting the installation."
+    sleep 5s
+}
+
+# The help function
+function display_help {
+    display_title "Help for the SFML library installation script"
+    display_flag "--help, -h"
+    display_default "\tDisplay this help message."
+    display_flag "--no-sleep, -n, -ns"
+    display_default "\tDo not sleep after showing the warning message."
+    display_flag "--silent, -s"
+    display_default "\tRun the script in silent mode."
+    display_flag "--verbose, -v"
+    display_default "\tRun the script in verbose mode."
+}
+
+# Process the arguments
+HELP_FOUND=$FALSE
+
+function process_input_arguments {
+    while [ $# -gt 0 ]; do
+        if [ "${1,,}" == "--silent" ] || [ "${1,,}" == "-s" ]; then
+            SILENT=$TRUE
+        elif [ "${1,,}" == "--verbose" ] || [ "${1,,}" == "-v" ]; then
+            SILENT=$FALSE
+        elif [ "${1,,}" == "--no-sleep" ] || [ "${1,,}" == "-n" ] || [ "${1,,}" == "-ns" ]; then
+            SLEEP=$FALSE
+        elif [ "${1,,}" == "--help" ] || [ "${1,,}" == "-h" ]; then
+            display_help
+            HELP_FOUND=$TRUE
+            return $SUCCESS
+        else
+            display_error "Unknown argument: $1"
+            display_help
+            return $ERROR
+        fi
+        shift
+    done
+}
+
 # The functions to install the SFML library
 function download_sfml {
     display_title "Downloading the SFML library..."
     mkdir -p $FILE_LOCATION
-    wget --progress=bar:force $SOURCE_CODE -O $FINAL_DOWNLOAD_PATH
+    if [ $SILENT -eq $TRUE ]; then
+        wget -q --show-progress=off --no-verbose $SOURCE_CODE -O $FINAL_DOWNLOAD_PATH >/dev/null
+    else
+        wget --progress=bar:force $SOURCE_CODE -O $FINAL_DOWNLOAD_PATH
+    fi
     STATUS=$?
     display_status $STATUS "Downloading the SFML library"
 }
 
 function extract_sfml {
     display_title "Extracting the SFML library..."
-    tar -xvzf $FINAL_DOWNLOAD_PATH -C $FILE_LOCATION
+    if [ $SILENT -eq $TRUE ]; then
+        unzip -o -q $FINAL_DOWNLOAD_PATH -d $FILE_LOCATION
+    else
+        unzip -o -v $FINAL_DOWNLOAD_PATH -d $FILE_LOCATION
+    fi
     STATUS=$?
     display_status $STATUS "Extracting the SFML library to $FILE_LOCATION"
 }
@@ -99,17 +189,32 @@ function extract_sfml {
 function compile_sfml {
     display_title "Compiling the SFML library..."
     cd $FILE_LOCATION/SFML-2.6.2
-    cmake .
+    if [ $SILENT -eq $TRUE ]; then
+        cmake . . >/dev/null
+    else
+        cmake . .
+    fi
     STATUS=$?
     display_status $STATUS "Configuring the SFML library"
-    make
+    if [ $STATUS -ne $SUCCESS ]; then
+        return
+    fi
+    if [ $SILENT -eq $TRUE ]; then
+        make all >/dev/null
+    else
+        make all
+    fi
     STATUS=$?
     display_status $STATUS "Compiling the SFML library"
 }
 
 function install_sfml {
     display_title "Installing the SFML library..."
-    $SUDO make install
+    if [ $SILENT -eq $TRUE ]; then
+        make install >/dev/null
+    else
+        make install
+    fi
     STATUS=$?
     display_status $STATUS "Installing the SFML library"
 }
@@ -122,18 +227,54 @@ function clean_up {
 }
 
 function main {
+    if [ $SILENT -eq $TRUE ]; then
+        display_info "The script is running in silent mode."
+    else
+        display_info "The script is running in verbose mode."
+    fi
     download_sfml
+    if [ $GLOBAL_STATUS -eq $ERROR ]; then
+        display_error "An error occurred during the download of the SFML library."
+        clean_up
+        return
+    fi
     extract_sfml
+    if [ $GLOBAL_STATUS -eq $ERROR ]; then
+        display_error "An error occurred during the extraction of the SFML library."
+        clean_up
+        return
+    fi
     compile_sfml
+    if [ $GLOBAL_STATUS -eq $ERROR ]; then
+        display_error "An error occurred during the compilation of the SFML library."
+        clean_up
+        return
+    fi
     install_sfml
-    clean_up
+    if [ $GLOBAL_STATUS -eq $ERROR ]; then
+        display_error "An error occurred during the installation of the SFML library."
+        clean_up
+        return
+    fi
+    # clean_up
 }
 
-main
-if [ $GLOBAL_STATUS -eq $ERROR ]; then
-    display_error "An error occurred during the installation of the SFML library."
+process_input_arguments $@
+STATUS=$?
+update_global_status $STATUS
+if [ $HELP_FOUND -eq $TRUE ]; then
+    display_default "End of script"
+    exit $SUCCESS
 fi
-display_info "The global status is $GLOBAL_STATUS"
-display_status $GLOBAL_STATUS "The installation of the SFML library"
-display_default "The installation of the SFML library has ended."
+if [ $GLOBAL_STATUS -eq $SUCCESS ]; then
+    relaunch_as_root $@
+    display_package_warning $@
+    main
+fi
+if [ $GLOBAL_STATUS -eq $ERROR ] && [ $STATUS -eq $SUCCESS ]; then
+    display_error "An error occurred during the installation of the SFML library."
+    display_info "The global status is $GLOBAL_STATUS"
+    display_status $GLOBAL_STATUS "The installation of the SFML library"
+fi
+display_default "End of script"
 exit $GLOBAL_STATUS
