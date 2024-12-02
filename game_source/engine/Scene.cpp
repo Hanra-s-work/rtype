004ca2ed687b511c9fdfea9a9c00480c65bc9c5d
Arthur Sardella <arthur-agusto.claro-sardella@epitech.eu>
2024-12-02 21:08:57 +0100

    add: scene Instatiating and destruction of object, and static current scene

--- game_source/engine/Scene.cpp
+++ game_source/engine/Scene.cpp
@@ -1,8 +1,19 @@
 #include "Scene.hpp"
+#include "Game.hpp"
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Graphics/Rect.hpp>
 
 bool Scene::checkCollision(GameObject &first, GameObject &second)
 {
     return first.sprite.getGlobalBounds().intersects(second.sprite.getGlobalBounds());
+}
+
+void Scene::Instatiate(GameObject &obj)
+{
+    Game::currentScene.addObject(obj);
+}
+
+void Scene::DestroyObject(GameObject &obj)
+{
+    Game::currentScene.removeObject(obj);
 }
--- game_source/engine/Scene.hpp
+++ game_source/engine/Scene.hpp
@@ -4,11 +4,22 @@
 #include <SFML/Graphics/Texture.hpp>
 #include "GameObject.hpp"
 
+/**
+ * @class Scene
+ * @brief Manages a collection of game objects and their interactions within a scene.
+ *
+ * The Scene class represents a game environment, containing a background texture, 
+ * a list of game objects, and operations like adding, removing, and checking collisions 
+ * between objects. It also provides static methods for object instantiation and destruction.
+ */
 class Scene {
 public:
     sf::Texture background;
     std::list<GameObject> objects;
 
     void addObject(GameObject &obj) { objects.push_front(obj); }
+    void removeObject(GameObject &obj) { objects.remove(obj); }
     bool checkCollision(GameObject &first, GameObject &second);
+    static void Instatiate(GameObject &obj);
+    static void DestroyObject(GameObject &obj);
 };
