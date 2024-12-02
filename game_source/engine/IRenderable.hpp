407bbfcd2d75ddcf21301390e39b6f69121eaa23
Arthur Sardella <arthur-agusto.claro-sardella@epitech.eu>
2024-12-02 20:36:59 +0100

    add: documentation for IRenderable

--- game_source/engine/IRenderable.hpp
+++ game_source/engine/IRenderable.hpp
@@ -2,6 +2,10 @@
 
 #include <SFML/Graphics/Sprite.hpp>
 
+/**
+ * @class IRenderable
+ * @brief Interface for classes that can be rendered on the window.
+ */
 class IRenderable {
     virtual sf::Sprite &GetSprite(void) = 0;
 };
