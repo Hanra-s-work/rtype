d94a86c86e93f9be74b61a047ceb02e4c1717568
Arthur Sardella <arthur-agusto.claro-sardella@epitech.eu>
2024-12-05 22:19:12 +0100

    mod: removed functions from .hpp files to .cpp

---
+++ game_source/systems/MovementSystem.cpp
@@ -0,0 +1,13 @@
+#include "MovementSystem.hpp"
+
+void movement_system(Registry &r) {
+    auto &positions = r.get_components<Position>();
+    auto &velocities = r.get_components<Velocity>();
+
+    for (auto &&[pos, vel] : Zipper(positions, velocities)) {
+        if (pos && vel) {
+            pos->X += vel->vX;
+            pos->Y += vel->vY;
+        }
+    }
+}
--- game_source/systems/MovementSystem.hpp
+++ game_source/systems/MovementSystem.hpp
@@ -5,14 +5,4 @@
 #include "Position.hpp"
 #include "Velocity.hpp"
 
-void movement_system(Registry &r) {
-    auto &positions = r.get_components<Position>();
-    auto &velocities = r.get_components<Velocity>();
-
-    for (auto &&[pos, vel] : Zipper(positions, velocities)) {
-        if (pos && vel) {
-            pos->X += vel->vX;
-            pos->Y += vel->vY;
-        }
-    }
-}
+void movement_system(Registry &r);
