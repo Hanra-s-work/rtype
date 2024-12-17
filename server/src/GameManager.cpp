4b0af5b5df0aff31acf9351a379d1eadbec18507
Arthur Sardella <arthur-agusto.claro-sardella@epitech.eu>
2024-12-17 15:19:17 +0100

    del: removed game-logic from server's cmake and reverted my modifications

--- server/src/GameManager.cpp
+++ server/src/GameManager.cpp
@@ -6,15 +6,12 @@
 */
 
 #include "./includes/GameManager.hpp"
-#include "Registry.hpp"
 #include <iostream>
 
 void GameManager::Initialize() {
     std::cout << "Initializing game manager in progress" << std::endl;
-    game = new Registry();
 }
 
 void GameManager::Update(float deltaTime) {
-    std::cout << "Updating game logic with deltaTime: " << deltaTime << "s." << std::endl;
-    game->run_systems();
+    //std::cout << "Updating game logic with deltaTime: " << deltaTime << "s." << std::endl;
 }
