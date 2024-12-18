0b8eea548cac6436515c585716917955b6eb81f1
Henry LETELLIER <henry.letellier@epitech.eu>
2024-12-18 02:26:22 +0100

    [UPDATE] the component list

--- gui/include/GUI/ECS/Components.hpp
+++ gui/include/GUI/ECS/Components.hpp
@@ -13,8 +13,9 @@
 #pragma once
 #include "Components/TextComponent.hpp"
 #include "Components/MusicComponent.hpp"
+#include "Components/ShapeComponent.hpp"
 #include "Components/ButtonComponent.hpp"
-#include "Components/RenderComponent.hpp"
+#include "Components/TextureComponent.hpp"
 #include "Components/PositionComponent.hpp"
 #include "Components/AnimationComponent.hpp"
 #include "Components/CollisionComponent.hpp"
