b1ee250e40a8e1e836870ccd219e3c03176fd992
Henry LETELLIER <henry.letellier@epitech.eu>
2024-12-31 14:56:44 +0100

    [UPDATE] unCast to accept custom errors for throwing and custom messages to prepend

--- gui/include/Utilities/unCast.hpp
+++ gui/include/Utilities/unCast.hpp
@@ -10,6 +10,7 @@
 #include <any>
 #include <string>
 #include <optional>
+#include <type_traits>
 
 #include "Log.hpp"
 #include "LogMacros.hpp"
@@ -18,26 +19,41 @@
 namespace Utilities
 {
     /**
-     * @brief A function in charge of casting the content of std::any back to it's original state.
+     * @brief A function in charge of casting the content of std::any back
+     * to it's original state.
      *
-     * @tparam T
+     * @tparam T The expected type
+     * @tparam Exception The custom type to throw,
+     * Default value: CustomExceptions::InvalidType
+     *
+     * @note The Exception must be able to accept and work
+     * if only a single parameter is passed because unCast will
+     * ignore the rest and put itself in the first position.
      *
      * @param classNode The entity to process
      * @param raiseOnError If uncasting failed, raise an error? (Default: true)
+     * @param customErrorMessage If the casting failed and the error was set
+     * to raise, prepend this message to the any cast raised error
+     *
+     * @note If not custom error message is passed, the `what` of
+     *  the any cast error is passed as is (it just get's cast to an std::string)
      *
      * @return T The un-casted member.
      */
-    template<typename T>
-    std::optional<T> unCast(const std::any &classNode, const bool raiseOnError = true)
+    template<typename T, typename Exception = CustomExceptions::InvalidType>
+    std::optional<T> unCast(const std::any &classNode, const bool raiseOnError = true, const std::string customErrorMessage = "")
     {
         try {
             return std::optional(std::any_cast<T>(classNode));
         }
         catch (std::bad_any_cast &e) {
             if (raiseOnError) {
-                throw CustomExceptions::InvalidType(std::string(e.what()));
+                throw Exception(customErrorMessage + std::string(e.what()));
             } else {
-                PRECISE_WARNING << "Any cast failed, system error: " + std::string(e.what()) << std::endl;
+                PRECISE_WARNING << "Any cast failed, Custom error message: '"
+                    << customErrorMessage
+                    << "', system error: " << std::string(e.what())
+                    << std::endl;
                 return std::nullopt;
             }
         }
