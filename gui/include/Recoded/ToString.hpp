55341878dd4ecb1ef4d22e0278241677048108a7
Henry LETELLIER <henry.letellier@epitech.eu>
2025-01-09 02:04:30 +0100

    [ADD] implement myToString functions for std::map, std::set, std::vector, std::unordered_map, and std::unordered_set to enhance string representation capabilities

--- gui/include/Recoded/ToString.hpp
+++ gui/include/Recoded/ToString.hpp
@@ -15,10 +15,21 @@
  */
 
 #pragma once
-#include <utility>
+#include <map>
+#include <set>
+#include <vector>
 #include <string>
+#include <utility>
+#include <unordered_set>
+#include <unordered_map>
 
+#include "ActiveScreen.hpp"
 #include "Recoded/Rect.hpp"
+#include "GUI/ECS/GameComponents/Package.hpp"
+#include "GUI/ECS/GameComponents/ECSPackage.hpp"
+#include "GUI/ECS/GameComponents/EntityType.hpp"
+#include "GUI/ECS/GameComponents/EntityActions.hpp"
+#include "GUI/ECS/GameComponents/EntityActionType.hpp"
 
 namespace Recoded
 {
@@ -31,6 +42,8 @@ namespace Recoded
      * @param value The boolean value to convert.
      *
      * @return A string representation of the boolean ("true" or "false").
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
      */
     const std::string myToString(bool value);
 
@@ -46,6 +59,8 @@ namespace Recoded
      * @param value The value to convert.
      *
      * @return A string representation of the numeric value.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
      */
     template <typename T>
     const std::string myToString(const T &value)
@@ -71,6 +86,8 @@ namespace Recoded
      * @param value The pair to convert.
      *
      * @return A string representation of the pair in the format `( x: <first>, y: <second> )`.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
      */
     template <typename T>
     const std::string myToString(const std::pair<T, T> &value)
@@ -100,14 +117,274 @@ namespace Recoded
      *
      * @return A string representation of the rectangle in the format
      *         `( x: <x>, y: <y>, width: <width>, height: <height> )`.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
      */
     template <typename T>
     const std::string myToString(const Rect<T> &rectangle)
     {
-        return "( x: " + myToString(rectangle.position.first)
-            + ", y: " + myToString(rectangle.position.second)
-            + ", width: " + myToString(rectangle.size.first)
-            + ", height: " + myToString(rectangle.size.second)
-            + ")";
+        std::string result = "( x: " + myToString(rectangle.position.first);
+        result += ", y: " + myToString(rectangle.position.second);
+        result += ", width: " + myToString(rectangle.size.first);
+        result += ", height: " + myToString(rectangle.size.second) + ")";
+        return result;
     }
+
+    /**
+     * @brief Converts a std::map to a string representation.
+     *
+     * This function iterates over the key-value pairs of a `std::map` and generates
+     * a string in the format: `{ 'key1' : 'value1', 'key2' : 'value2', ... }`.
+     *
+     * @tparam Key The type of the keys in the map.
+     * @tparam Value The type of the values in the map.
+     *
+     * @param map The map to be converted to a string.
+     *
+     * @return A string representing the map.
+     *
+     * @note Uses `Recoded::myToString` to convert keys and values to strings.
+     */
+    template<typename Key, typename Value>
+    std::string myToString(const std::map<Key, Value> &map)
+    {
+        std::string result = "{ ";
+        typename std::map<Key, Value>::const_iterator it = map.begin();
+        for (; it != map.end(); ++it) {
+            result += "'" + Recoded::myToString(it->first) + "' : '" + Recoded::myToString(it->second) + "'";
+            if (std::next(it) != map.end())
+                result += ", ";
+        }
+        result += " }";
+        return result;
+    }
+
+    /**
+     * @brief Converts a std::set to a string representation.
+     *
+     * This function iterates over the elements of a `std::set` and generates
+     * a string in the format: `{ 'element1', 'element2', ... }`.
+     *
+     * @tparam T The type of the elements in the set.
+     *
+     * @param set The set to be converted to a string.
+     *
+     * @return A string representing the set.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
+     */
+    template<typename T>
+    std::string myToString(const std::set<T> &set)
+    {
+        std::string result = "{ ";
+        for (typename std::set<T>::const_iterator it = set.begin(); it != set.end();) {
+            result += "'" + Recoded::myToString(*it) + "'";
+            if (++it != set.end())
+                result += ", ";
+        }
+        result += " }";
+        return result;
+    }
+
+    /**
+     * @brief Converts a std::vector to a string representation.
+     *
+     * This function iterates over the elements of a `std::vector` and generates
+     * a string in the format: `[ 'element1', 'element2', ... ]`.
+     *
+     * @tparam T The type of the elements in the vector.
+     *
+     * @param vec The vector to be converted to a string.
+     *
+     * @return A string representing the vector.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
+     */
+    template<typename T>
+    const std::string myToString(const std::vector<T> &set)
+    {
+        std::string result = "[ ";
+        typename std::vector<T>::const_iterator it = set.begin();
+        for (; it != set.end(); ++it) {
+            result += "'" + Recoded::myToString(*it) + "'";
+            if (std::next(it) != set.end())
+                result += ", ";
+        }
+        result += " ]";
+        return result;
+    }
+
+    /**
+     * @brief Converts a std::unordered_map to a string representation.
+     *
+     * This function iterates over the key-value pairs of a `std::unordered_map` and generates
+     * a string in the format: `{ 'key1' : 'value1', 'key2' : 'value2', ... }`.
+     *
+     * @tparam Key The type of the keys in the unordered map.
+     * @tparam Value The type of the values in the unordered map.
+     *
+     * @param map The unordered map to be converted to a string.
+     *
+     * @return A string representing the unordered map.
+     *
+     * @note Uses `Recoded::myToString` to convert keys and values to strings.
+     */
+    template<typename Key, typename Value>
+    std::string myToString(const std::unordered_map<Key, Value> &map)
+    {
+        std::string result = "{ ";
+        typename std::unordered_map<Key, Value>::const_iterator it = map.begin(); // Correct variable name
+        for (; it != map.end(); ++it) {
+            result += "'" + Recoded::myToString(it->first) + "' : '" + Recoded::myToString(it->second) + "'";
+            if (std::next(it) != map.end()) // Avoid trailing comma
+                result += ", ";
+        }
+        result += " }";
+        return result;
+    }
+
+    /**
+     * @brief Converts a std::unordered_set to a string representation.
+     *
+     * This function iterates over the elements of a `std::unordered_set` and generates
+     * a string in the format: `{ 'element1', 'element2', ... }`.
+     *
+     * @tparam T The type of the elements in the unordered set.
+     *
+     * @param set The unordered set to be converted to a string.
+     *
+     * @return A string representing the unordered set.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
+     */
+    template<typename T>
+    const std::string myToString(const std::unordered_set<T> &set)
+    {
+        std::string result = "{ ";
+        typename std::unordered_set<T>::const_iterator it = set.begin();
+        for (; it != set.end(); ++it) {
+            result += "'" + Recoded::myToString(*it) + "'";
+            if (std::next(it) != set.end())
+                result += ", ";
+        }
+        result += " }";
+        return result;
+    }
+
+    /**
+     * @brief Converts an `ActiveScreen` enumeration value to a string.
+     *
+     * This function maps each value of the `ActiveScreen` enum to a corresponding string representation.
+     *
+     * @param item The `ActiveScreen` enum value to convert.
+     *
+     * @return A string representation of the given `ActiveScreen` enum value.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
+     */
+    const std::string myToString(const ActiveScreen &item);
+
+    /**
+     * @brief Converts a `GUI::ECS::GameComponents::Package` structure to a string.
+     *
+     * This function generates a formatted string representing the details of the `Package` structure.
+     *
+     * @param item The `Package` structure to convert.
+     *
+     * @return A string containing the details of the given `Package`.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
+     */
+    const std::string myToString(const GUI::ECS::GameComponents::Package &item);
+
+    /**
+     * @brief Converts a `GUI::ECS::GameComponents::Package_t` structure to a string.
+     *
+     * This function generates a formatted string representing the details of the `Package_t` structure.
+     *
+     * @param item The `Package_t` structure to convert.
+     *
+     * @return A string containing the details of the given `Package_t`.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
+     */
+    const std::string myToString(const GUI::ECS::GameComponents::Package_t &item);
+
+    /**
+     * @brief Converts a `GUI::ECS::GameComponents::EntityType` enumeration value to a string.
+     *
+     * This function maps each value of the `EntityType` enum to a corresponding string representation.
+     *
+     * @param item The `EntityType` enum value to convert.
+     *
+     * @return A string representation of the given `EntityType` enum value.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
+     */
+    const std::string myToString(const GUI::ECS::GameComponents::EntityType &item);
+
+    /**
+     * @brief Converts a `GUI::ECS::GameComponents::ECSPackage` structure to a string.
+     *
+     * This function generates a formatted string representing the details of the `ECSPackage` structure.
+     *
+     * @param item The `ECSPackage` structure to convert.
+     *
+     * @return A string containing the details of the given `ECSPackage`.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
+     */
+    const std::string myToString(const GUI::ECS::GameComponents::ECSPackage &item);
+
+    /**
+     * @brief Converts a `GUI::ECS::GameComponents::ECSPackage_t` structure to a string.
+     *
+     * This function generates a formatted string representing the details of the `ECSPackage_t` structure.
+     *
+     * @param item The `ECSPackage_t` structure to convert.
+     *
+     * @return A string containing the details of the given `ECSPackage_t`.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
+     */
+    const std::string myToString(const GUI::ECS::GameComponents::ECSPackage_t &item);
+
+    /**
+     * @brief Converts a `GUI::ECS::GameComponents::EntityActions` structure to a string.
+     *
+     * This function generates a formatted string representing the details of the `EntityActions` structure.
+     *
+     * @param item The `EntityActions` structure to convert.
+     *
+     * @return A string containing the details of the given `EntityActions`.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
+     */
+    const std::string myToString(const GUI::ECS::GameComponents::EntityActions &item);
+
+    /**
+     * @brief Converts a `GUI::ECS::GameComponents::EntityActions_t` structure to a string.
+     *
+     * This function generates a formatted string representing the details of the `EntityActions_t` structure.
+     *
+     * @param item The `EntityActions_t` structure to convert.
+     *
+     * @return A string containing the details of the given `EntityActions_t`.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
+     */
+    const std::string myToString(const GUI::ECS::GameComponents::EntityActions_t &item);
+
+    /**
+     * @brief Converts a `GUI::ECS::GameComponents::EntityActionType` enumeration value to a string.
+     *
+     * This function maps each value of the `EntityActionType` enum to a corresponding string representation.
+     *
+     * @param item The `EntityActionType` enum value to convert.
+     *
+     * @return A string representation of the given `EntityActionType` enum value.
+     *
+     * @note Uses `Recoded::myToString` to convert elements to strings.
+     */
+    const std::string myToString(const GUI::ECS::GameComponents::EntityActionType &item);
 }
--- gui/src/Recoded/ToString.cpp
+++ gui/src/Recoded/ToString.cpp
@@ -20,3 +20,111 @@ const std::string Recoded::myToString(bool value)
     }
     return "false";
 };
+
+const std::string Recoded::myToString(const ActiveScreen &item)
+{
+    switch (item) {
+        case ActiveScreen::LOADING:
+            return "LOADING";
+            break;
+        case ActiveScreen::MENU:
+            return "MENU";
+            break;
+        case ActiveScreen::GAME:
+            return "GAME";
+            break;
+        case ActiveScreen::DEMO:
+            return "DEMO";
+            break;
+        case ActiveScreen::SETTINGS:
+            return "SETTINGS";
+            break;
+        case ActiveScreen::GAME_OVER:
+            return "GAME OVER";
+            break;
+        case ActiveScreen::BOSS_FIGHT:
+            return "BOSS FIGHT";
+            break;
+        case ActiveScreen::CONNECTION_FAILED:
+            return "CONNECTION FAILED";
+            break;
+        case ActiveScreen::CONNECTION_ADDRESS:
+            return "CONNECTION ADDRESS";
+            break;
+        default:
+            return "UNKNOWN";
+            break;
+    };
+};
+
+const std::string Recoded::myToString(const GUI::ECS::GameComponents::Package_t &item)
+{
+    std::string result = "Package_t: {\n";
+    result += "- GraphicID: " + Recoded::myToString(item.GraphicID) + "\n";
+    result += "- EntityID: " + Recoded::myToString(item.EntityID) + "\n";
+    result += "- posx: " + Recoded::myToString(item.posx) + "\n";
+    result += "- posy: " + Recoded::myToString(item.posy) + "\n";
+    result += "}\n";
+    return result;
+}
+
+const std::string Recoded::myToString(const GUI::ECS::GameComponents::EntityType &item)
+{
+    switch (item) {
+        case GUI::ECS::GameComponents::EntityType::PLAYER:
+            return "PLAYER";
+            break;
+
+        case GUI::ECS::GameComponents::EntityType::ENEMY:
+            return "ENEMY";
+            break;
+
+        case GUI::ECS::GameComponents::EntityType::LASER_ENEMY:
+            return "LASER_ENEMY";
+            break;
+
+        case GUI::ECS::GameComponents::EntityType::LASER_PLAYER:
+            return "LASER_PLAYER";
+            break;
+
+        default:
+            return "UNKNOWN";
+            break;
+    };
+};
+
+const std::string Recoded::myToString(const GUI::ECS::GameComponents::ECSPackage_t &item)
+{
+    std::string result = "ECSPackage_t: {\n";
+    result += "- EntityID: " + Recoded::myToString(item.EntityID) + "\n";
+    result += "- posx: " + Recoded::myToString(item.posx) + "\n";
+    result += "- posy: " + Recoded::myToString(item.posy) + "\n";
+    result += "}\n";
+    return result;
+}
+
+const std::string Recoded::myToString(const GUI::ECS::GameComponents::EntityActions_t &item)
+{
+    std::string result = "EntityActions_t: {\n";
+    result += "- type: " + Recoded::myToString(item.type) + "\n";
+    result += "- posx: " + Recoded::myToString(item.posx) + "\n";
+    result += "- posy: " + Recoded::myToString(item.posy) + "\n";
+    result += "}\n";
+    return result;
+}
+
+const std::string Recoded::myToString(const GUI::ECS::GameComponents::EntityActionType &item)
+{
+    switch (item) {
+        case GUI::ECS::GameComponents::EntityActionType::MOVE:
+            return "MOVE";
+            break;
+        case GUI::ECS::GameComponents::EntityActionType::SHOOT:
+            return "SHOOT";
+            break;
+
+        default:
+            return "UNKNOWN";
+            break;
+    }
+}
