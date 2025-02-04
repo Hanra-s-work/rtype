d8e3a30d924299c244509c8e4f0dd18d495e8da2
floks <tristan.wehrle@epitech.eu>
2025-02-04 20:22:04 +0000

    [REFACTOR] GUI for lisibility

--- new_project/client/include/EntityManager.hpp
+++ new_project/client/include/EntityManager.hpp
@@ -1,50 +1,25 @@
-//EntityManager.hpp
+#ifndef ENTITYMANAGER_HPP
+    #define ENTITYMANAGER_HPP
 
-#pragma once
-#include <unordered_map>
-#include <memory>
-#include "Entity.hpp"
-#include "HandleSprites.hpp"
-#include "../../common/NetworkProtocol.hpp" // Pour EntityType
-#include <unordered_set>
-#include <iostream>
+    #include <unordered_map>
+    #include <memory>
+    #include "Entity.hpp"
+    #include "SpriteEntity.hpp"
+    #include "../../common/NetworkProtocol.hpp"
+    #include <unordered_set>
 
-class EntityManager {
-public:
-    // Met à jour toutes les entités
-    void update(float dt);
-    
-    // Affiche toutes les entités dans la fenêtre
-    void render(sf::RenderWindow &window);
-    
-    // Met à jour une entité existante ou la crée si elle n'existe pas
-    void updateEntity(uint32_t entityId, EntityType type, float posX, float posY);
-    void removeEntity(uint32_t entityId);
-    void updateLife(uint32_t entityId, EntityType type, uint32_t life);
-    SpriteEntity* getSpriteEntity(uint32_t entityId);
-    // void printEntitiesPositions() const;
-    void printEntitiesSizes() const;
-private:
-    // Factory qui crée l'entité selon son type
-    std::unique_ptr<Entity> createEntity(EntityType type, float posX, float posY) {
-        // Selon le type d'entité, on charge un asset différent.
-        // Adapte les chemins et les classes si tu souhaites des comportements spécifiques.
-        switch (type) {
-            case EntityType::Player:
-                return std::make_unique<SpriteEntity>("client/assets/player.png", posX, posY);
-            case EntityType::Monster:
-                return std::make_unique<SpriteEntity>("client/assets/monster.png", posX, posY);
-            case EntityType::PlayerMissile:
-                return std::make_unique<SpriteEntity>("client/assets/p_missile.png", posX, posY);
-            case EntityType::MonsterMissile:
-                return std::make_unique<SpriteEntity>("client/assets/m_missile.png", posX, posY);
-            // Tu peux ajouter d'autres cas (Powerup, Boss, etc.)
-            default:
-                // En cas d'erreur, retourne un sprite par défaut
-                return std::make_unique<SpriteEntity>("client/assets/default.png", posX, posY);
-        }
-    }
-    
-    std::unordered_map<uint32_t, std::unique_ptr<Entity>> _entities;
-    std::unordered_set<uint32_t> _destroyedEntities;
-};
+    class EntityManager {
+        public:
+            void update(float dt);
+            void render(sf::RenderWindow &window);
+            void updateEntity(uint32_t entityId, EntityType type, float posX, float posY);
+            void removeEntity(uint32_t entityId);
+            void updateLife(uint32_t entityId, EntityType type, uint32_t life);
+            SpriteEntity* getSpriteEntity(uint32_t entityId);
+        private:
+            std::unique_ptr<Entity> createEntity(EntityType type, float posX, float posY);
+            std::unordered_map<uint32_t, std::unique_ptr<Entity>> _entities;
+            std::unordered_set<uint32_t> _destroyedEntities;
+    };
+
+#endif // ENTITYMANAGER_HPP
--- new_project/client/src/EntityManager.cpp
+++ new_project/client/src/EntityManager.cpp
@@ -1,93 +1,72 @@
-//EntityManager.cpp
-
+// EntityManager.cpp
 #include "EntityManager.hpp"
 
-// Met à jour toutes les entités
-void EntityManager::update(float dt) {
-    for (auto &pair : _entities) {
+void EntityManager::update(float dt)
+{
+    for (auto &pair : _entities)
         pair.second->update(dt);
-    }
 }
 
-// Affiche toutes les entités dans la fenêtre
-void EntityManager::render(sf::RenderWindow &window) {
-    for (auto &pair : _entities) {
+void EntityManager::render(sf::RenderWindow &window)
+{
+    for (auto &pair : _entities)
         pair.second->render(window);
-    }
 }
 
-void EntityManager::updateEntity(uint32_t entityId, EntityType type, float posX, float posY) {
-    // If the entity was previously destroyed, do not re-create it.
-    if (_destroyedEntities.find(entityId) != _destroyedEntities.end()) {
+void EntityManager::updateEntity(uint32_t entityId, EntityType type, float posX, float posY)
+{
+    if (_destroyedEntities.find(entityId) != _destroyedEntities.end())
         return;
-    }
-    
     auto it = _entities.find(entityId);
-    if (it == _entities.end()) {
-        // Entity does not exist: create it.
+    if (it == _entities.end())
         _entities[entityId] = createEntity(type, posX, posY);
-    } else {
-        // Update the existing entity.
+    else {
         auto* se = dynamic_cast<SpriteEntity*>(it->second.get());
-        if (se) {
+        if (se)
             se->setTargetPosition(posX, posY);
-        } else {
+        else
             it->second->setPosition(posX, posY);
-        }
     }
 }
 
-void EntityManager::removeEntity(uint32_t entityId) {
+void EntityManager::removeEntity(uint32_t entityId)
+{
     _entities.erase(entityId);
     _destroyedEntities.insert(entityId);
 }
 
-
-void EntityManager::updateLife(uint32_t entityId, EntityType type, uint32_t life) {
-    if (_destroyedEntities.find(entityId) != _destroyedEntities.end()) {
-    return;
-    }
-
+void EntityManager::updateLife(uint32_t entityId, EntityType type, uint32_t life)
+{
+    if (_destroyedEntities.find(entityId) != _destroyedEntities.end())
+        return;
     auto it = _entities.find(entityId);
     if (it != _entities.end()) {
-        // On suppose que l'entité est un SpriteEntity (par exemple un joueur ou un monstre)
         auto* spriteEntity = dynamic_cast<SpriteEntity*>(it->second.get());
-        if (spriteEntity) {
+        if (spriteEntity)
             spriteEntity->setLife(life);
-        }
     }
 }
 
-SpriteEntity* EntityManager::getSpriteEntity(uint32_t entityId) {
+SpriteEntity* EntityManager::getSpriteEntity(uint32_t entityId)
+{
     auto it = _entities.find(entityId);
-    if (it != _entities.end()) {
+    if (it != _entities.end())
         return dynamic_cast<SpriteEntity*>(it->second.get());
-    }
     return nullptr;
 }
 
-void EntityManager::printEntitiesSizes() const {
-    std::cout << "Taille des entités dans le jeu :" << std::endl;
-    for (const auto &pair : _entities) {
-        // On suppose ici que toutes les entités sont des SpriteEntity.
-        // On peut utiliser dynamic_cast pour vérifier.
-        const SpriteEntity* spriteEntity = dynamic_cast<const SpriteEntity*>(pair.second.get());
-        if (spriteEntity) {
-            sf::Vector2f size = spriteEntity->getSize();
-            std::cout << "Entity ID: " << pair.first 
-                      << " - Taille: (" << size.x << ", " << size.y << ")" 
-                      << std::endl;
-        } else {
-            std::cout << "Entity ID: " << pair.first << " n'est pas une SpriteEntity." << std::endl;
-        }
+std::unique_ptr<Entity> EntityManager::createEntity(EntityType type, float posX, float posY)
+{
+    switch (type) {
+        case EntityType::Player:
+            return std::make_unique<SpriteEntity>("client/assets/player.png", posX, posY);
+        case EntityType::Monster:
+            return std::make_unique<SpriteEntity>("client/assets/monster.png", posX, posY);
+        case EntityType::PlayerMissile:
+            return std::make_unique<SpriteEntity>("client/assets/p_missile.png", posX, posY);
+        case EntityType::MonsterMissile:
+            return std::make_unique<SpriteEntity>("client/assets/m_missile.png", posX, posY);
+        default:
+            return std::make_unique<SpriteEntity>("client/assets/default.png", posX, posY);
     }
 }
-
-// void EntityManager::printEntitiesPositions() const {
-//     for (const auto &pair : _entities) {
-//         sf::Vector2f pos = pair.second->getPosition();
-//         std::cout << "Entity " << pair.first 
-//                   << " position: (" << pos.x << ", " << pos.y << ")" 
-//                   << std::endl;
-//     }
-// }
